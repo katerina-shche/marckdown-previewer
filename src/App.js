ef702b8259f8342c443ffe9406034a9ead87608e
katerina-shche <asti.katerina@gmail.com>
2023-04-24 11:28:04 +0200

    some markdown content added into js string

--- src/App.js
+++ src/App.js
@@ -6,15 +6,22 @@ import './App.css'
 
 
 export default function App() {
-  const str = "# Welcome to my React Markdown Previewer! \n\n ## This is a sub-heading...\n\n ### And here's some other cool stuff: \n\n Heres some code, `<div></div>`, between 2 backticks.\n\n ```
-  // this is multi-line code:
-  
-  function anotherExample(firstLine, lastLine) {
-    if (firstLine == '```' && lastLine == '```') {
-      return multiLineCode;
-    }
-  }
-  ```"
+  const str = `# Welcome to my React Markdown Previewer!
+   ## This is a sub-heading...
+   ### And here's some other cool stuff:
+   Heres some code, \`<div></div>\`, between 2 backticks.
+   \`\`\`javascript
+   // this is multi-line code:
+      function anotherExample(firstLine, lastLine) {
+        if (firstLine == \"\`\`\`\" && lastLine == \"\`\`\`\") {
+          return multiLineCode;
+        }
+      };
+  \`\`\`
+  You can also make text **bold**... whoa!\<br\>
+  Or _italic_.\<br\>
+  Or... wait for it... **_both!_**\<br\>
+  And feel free to go crazy ~~crossing stuff out~~.`
   
 
   const [text, setText] = useState(str);
