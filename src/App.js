d4de44ccd8720219af03522a1ea2da3ed8ecc314
katerina-shche <asti.katerina@gmail.com>
2023-04-25 13:25:08 +0200

    js is done

--- src/App.js
+++ src/App.js
@@ -34,6 +34,17 @@ Wild Header | Crazy Header | Another Header?
 Your content can | be here, and it | can be here....
 And here. | Okay. | I think we get it.
 
+- And of course there are lists.
+- Some are bulleted.
+  - With different indentation levels.
+    - That look like this.
+
+1. And there are numbered lists too.
+1. Use just 1s if you want!
+1. And last but not least, let's not forget embedded images:
+
+![freecodecamp logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)
+
   `
   
 
